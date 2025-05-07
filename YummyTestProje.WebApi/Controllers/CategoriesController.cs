cea84c1189711b1db2a892ec08bf2b0dd8025e4b
FerhatFeyzullah <ferhatfeyzullah@hotmail.com>
2025-05-07 16:17:24 +0300

    Add project files.

---
+++ YummyTestProje.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YummyTestProje.WebApi.Context;
+using YummyTestProje.WebApi.Entities;
+
+namespace YummyTestProje.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori Ekleme Islemi Basarili");
+        }
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var value = _context.Categories.ToList();
+            return Ok(value);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori Silme Islemi Basarili");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori Guncelleme Islemi Basarili");
+        }
+
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id) 
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+    }
+}
---
+++ YummyTestProje.WebApi/Entities/Chef.cs
@@ -0,0 +1,11 @@
+﻿namespace YummyTestProje.WebApi.Entities
+{
+    public class Chef
+    {
+        public int ChefId { get; set; }
+        public string NameSurname { get; set; }
+        public string title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
